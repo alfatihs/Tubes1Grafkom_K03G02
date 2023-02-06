6b3825d7a9b5b0aabbd5d464870f3149296b215e
Azmi Alfatih S <73034464+alfatihs@users.noreply.github.com>
2023-02-06 22:39:32 +0700

    Create Model.js

---
+++ src/Model.js
@@ -0,0 +1,11 @@
+//Setiap bangun (Line, Square, Rectangle) harus bisa jalanin ini
+class Model {
+  constructor(id){
+    this.vertices = [];
+    this.type = "Model";
+    this.name = "Nameless Model";
+    this.center = new Point([0,0], [0,0,0,1]) //Titik berat
+    this.preserveSimilarity = true;
+    this.isFan = true;
+    this.id = id;
+  }
