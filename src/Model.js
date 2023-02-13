9d04d1ecefb107cb680c21a6794153e4f8e4045a
Mahesa Lizardy <88304455+lizardyy@users.noreply.github.com>
2023-02-13 16:15:46 +0700

    feat: template model

--- src/Model.js
+++ src/Model.js
@@ -2,50 +2,67 @@
 
 var maxNumTriangles = 200;
 var maxNumVertices = 3 * maxNumTriangles;
+var cBuffer;
+var vColor;
+var vBuffer;
+var vPosition;
+var vColor;
+// pisah polygon 
 class Model {
   constructor(id, name){
     this.vertices = [];
-    this.name = name;
-    this.preserveSimilarity = true;
-    this.isFan = true;
     this.id = id;
   }
-
   addVertex(vertex){
     this.vertices.push(vertex);
   }
 
-  render = (gl) => {
+  changeLastVertex(vertex){
+    if (this.vertices.length!=0){
+      this.vertices[this.vertices.length-1] = vertex;
+    }
+  }
+  render(gl){
     const vertices = [];
     for (let j = 0; j < this.vertices.length; j++) {
       vertices.push(this.vertices[j]);
     }
     
-    var vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
 
-    var vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
-    // for color
-    var cBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, 16 * maxNumVertices, gl.STATIC_DRAW);
 
-    var vColor = gl.getAttribLocation(program, "vColor");
+  
     gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vColor);
-
-    if(this.name =="Polygon"){
-      gl.drawArrays(gl.TRIANGLE_FAN, 0, this.vertices.length);
-    }
-    else if(this.name == "Line"){
-      gl.drawArrays(gl.LINES, 0, this.vertices.length);
-    }
-    else{
-      gl.drawArrays(gl.TRIANGLES, 0, this.vertices.length);
-    }
   }
+}
+
+class Polygon extends Model{
+
+  constructor(id){
+    super(id, id + ". Polygon");
+  }
+
+  render(gl){
+    super.render(gl);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, this.vertices.length);
+  }
+}
+
+class Square extends Model {
+
+}
+
+class Rectangle extends Model {
+
+}
+
+class Line extends Model {
+  
 }
--- src/script.js
+++ src/script.js
@@ -7,8 +7,9 @@ var draw;
 var maxNumTriangles = 200;
 var maxNumVertices = 3 * maxNumTriangles;
 var index = 0;
-var program
-
+var program;
+var afterClick = false;
+var firstClick = true;
 const model = new Model(0,"Line");
 const arrayObject = [model];
 
@@ -30,25 +31,50 @@ window.onload = function init() {
     gl.clear(gl.COLOR_BUFFER_BIT);
 
 
+    cBuffer = gl.createBuffer();
+    vBuffer = gl.createBuffer();
+    vPosition = gl.getAttribLocation(program, "vPosition");
+    vColor = gl.getAttribLocation(program, "vColor");
+
+    // event listener mouse klik
     canvas.addEventListener("click", function (event) {
+        if (firstClick){
+            var point = vec2(2 * event.clientX / canvas.width - 1,
+                2 * (canvas.height - event.clientY) / canvas.height - 1);
+            arrayObject[arrayObject.length - 1].addVertex(point);
+            firstClick = false;
+        }
+        afterClick = true;
+        render()
+    });
+
+    canvas.addEventListener("mousemove", function (event) {
         var point = vec2(2 * event.clientX / canvas.width - 1,
             2 * (canvas.height - event.clientY) / canvas.height - 1);
+        if (afterClick){
+            arrayObject[arrayObject.length - 1].addVertex(point);
+            afterClick =false;
+        }else{
+            arrayObject[arrayObject.length - 1].changeLastVertex(point);
+            
+        }
         
-        arrayObject[arrayObject.length-1].addVertex(point);
-
-        render();
     });
+
 
     document.getElementById("draw").onchange = function () {
         var value = document.getElementById("draw").value;
-        if (value == "Polygon"){
-            arrayObject.push(new Model(arrayObject.length, "Polygon"))
-        }
-        else if(value == "Line"){
-            arrayObject.push(new Model(arrayObject.length, "Line"))
+        if(value == "Line"){
+            arrayObject.push(new Line(arrayObject.length))
         }
         else if (value == "Square") {
-            arrayObject.push(new Model(arrayObject.length, "Square"))
+            arrayObject.push(new Square(arrayObject.length))
+        }
+        else if (value == "Rectangle") {
+            arrayObject.push(new Rectangle(arrayObject.length))
+        }
+        else if (value == "Polygon") {
+            arrayObject.push(new Polygon(arrayObject.length))
         }
     };
 }
@@ -56,6 +82,7 @@ window.onload = function init() {
 
 function render() {
     gl.clear(gl.COLOR_BUFFER_BIT);
+
     for (let i = 0; i < arrayObject.length; i++) {
         arrayObject[i].render(gl);
     }
